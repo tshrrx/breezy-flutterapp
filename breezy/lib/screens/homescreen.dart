6defbc8c8887a79b64fee05187e654e03fe7e5d7
tshrrx <144604940+tshrrx@users.noreply.github.com>
2024-07-01 22:10:08 +0530

    Update homescreen.dart

---
+++ breezy/lib/screens/homescreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget{
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<Homescreen> {
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
